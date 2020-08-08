33ecbc20922b3c1ba771fa37a098202e01fb6852
sorikiki <sorikiki98@naver.com>
2020-08-08 22:20:14 +0900

    redux fundamentals splitting reducers

--- redux_essentials_example/.redux_basic/4. Redux fundamentals.js
+++ redux_essentials_example/.redux_basic/4. Redux fundamentals.js
@@ -173,3 +173,167 @@ export function setVisibilityFilter(filter) {
 // ✔ separate action creators from reducer functions and export them to component.
 // ✔ reducer functions import action types and make logic without mutating state.
 
+
+// ✅ Splitting Reducers
+// This is called 'reducer composition', and it's the fundamental pattern of building Redux apps.
+
+/* Before Splitting
+  function todoApp(state = initialState, action) {
+    switch (action.type) {
+      case SET_VISIBILITY_FILTER:
+        return Object.assign({}, state, {
+          visibilityFilter: action.filter
+        })
+      case ADD_TODO:
+        return Object.assign({}, state, {
+          todos: [
+            ...state.todos,
+            {
+              text: action.text,
+              completed: false
+            }
+          ]
+        })
+      case TOGGLE_TODO:
+        return Object.assign({}, state, {
+          todos: state.todos.map((todo, index) => {
+            if (index === action.index) {
+              return Object.assign({}, todo, {
+                completed: !todo.completed
+              })
+            }
+            return todo
+          })
+        })
+      default:
+        return state
+    }
+  }
+*/
+
+/* Extract todos from a main reducer.
+  function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      })
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
+      })
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
+      })
+    default:
+      return state
+  }
+}
+*/
+
+/* Extract both todos and visibilityFilter from a main reducer.
+  function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+    default:
+      return state
+  }
+}
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter
+    default:
+      return state
+  }
+}
+
+function todoApp(state = {}, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    todos: todos(state.todos, action)
+  }
+}
+
+*/
+
+// Note that each of these reducers is managing its own part of the global state. 
+// The state parameter is different for every reducer, and corresponds to the part of the state it manages.
+
+
+// ✅ combineReducers()
+// And like other reducers, combineReducers() does not create a new object if all of the reducers provided to it do not change state.
+
+/*
+  import { combineReducers } from 'redux'
+
+  const todoApp = combineReducers({
+    visibilityFilter,
+    todos
+  })
+
+  export default todoApp
+*/
+
+// This is same with...
+
+/*
+  export default function todoApp(state = {}, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    todos: todos(state.todos, action)
+  }
+}
+*/
+
+// ✔ Note for ES6 new syntax
+// Because combineReducers expects an object, we can put all top-level reducers into a separate file, 
+// export each reducer function, and use import * as reducers to get them as an object with their names as the keys.
+/*
+  import { combineReducers } from 'redux'
+  import * as reducers from './reducers'
+
+  const todoApp = combineReducers(reducers)
+*/
