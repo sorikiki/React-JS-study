816f25127c81c3d1fba485ec14f8596b084b3bac
sorikiki <sorikiki98@naver.com>
2020-08-11 22:31:00 +0900

    redux middleware async flow

--- redux_essentials_example/.redux_basic/4. Redux  middleware.js
+++ redux_essentials_example/.redux_basic/4. Redux  middleware.js
@@ -4,51 +4,17 @@
 // - An action informing the reducers that the request finished successfully.
 // - An action informing the reducers that the request failed.
 
-/*
-    const LOGIN_REQUEST = 'user/LOGIN_REQUEST';
-    const LOGIN_SUCCESS = 'user/LOGIN_SUCCESS';
-    const LOGIN_ERROR = 'user/LOGIN_ERROR';
-
-    export const logInLoading = (id, password) => ({
-        type: LOGIN_REQUEST,
-        id,
-        password
-    })
-
-    export const logInSuccess = () => ({
-        type: LOGIN_SUCCESS
-    })
-
-    export const logInError = () => ({
-        type: LOGIN_ERROR,
-        error: '등록되지 않은 회원입니다.'
-    })
-*/
 
 // ✅ Async action creators using 'thunks!'
 // ❓ how do we use the synchronous action creators we defined earlier together with network requests?
 // ❗ The standard way to do it with Redux is to use the Redux Thunk middleware.
+// ✨ The redux-thunk middleware, which allows simple asynchronous use of dispatch.
 // => by using this specific middleware, an action creator can return a function instead of an action object.
 // => This way, the action creator becomes a thunk.
 // => This function doesn't need to be pure; it is thus allowed to have side effects, including executing asynchronous API calls.
 // => The function can also dispatch actions—like those synchronous actions we defined earlier.
 
-/*
-    const LOGIN_REQUEST = 'user/LOGIN_REQUEST';
-    const LOGIN_SUCCESS = 'user/LOGIN_SUCCESS';
-    const LOGIN_ERROR = 'user/LOGIN_ERROR';
 
-    export const logInLoading = (id, password) => (state, dispatch)=> {
-        if(state.user.id === id && state.user.password === password) {
-            return setTimeout(()=> dispatch(logInSuccess()), 1000)}
-        else return setTimeout(() => logInError(), 1000);
-    }
-
-    const logInSuccess = () => ({
-        type: LOGIN_SUCCESS
-    })
-
-    const logInError = () => {
-        alert('등록되지 않은 회원입니다.')
-    }
-*/
+// ✅ Async Flow
+// It provides a third-party extension point between dispatching an action, and the moment it reaches the reducer. 
+// ex. people use Redux middleware for logging, crash reporting, talking to an asynchronous API, routing, and more.
