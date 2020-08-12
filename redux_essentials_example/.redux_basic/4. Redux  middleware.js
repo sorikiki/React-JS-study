3ed0e23095a319b2b3141e169969407f7878b0b4
sorikiki <sorikiki98@naver.com>
2020-08-12 14:57:47 +0900

    redux middleware basic

--- redux_essentials_example/.redux_basic/4. Redux  middleware.js
+++ redux_essentials_example/.redux_basic/4. Redux  middleware.js
@@ -1,3 +1,12 @@
+// ✅ Middleware 
+// action => middleware1 => next => middleware2 => next => reducer => store
+
+// ✔ 'next' is a form of parameter function. ex. next(parameter)
+// => It is similar to store.dispatch! However next pass its action to middleware or reducer(if were not middleware)❗
+// ✔ If we use store.dispatch interally, action will be start from the first middleware. 
+// ✔ When you want to logging, install 'redux-logger' which covers console with colors and shows dispatched time.
+
+
 // ✅ Synchronous action creators
 // => usually, for any API request you'll want to dispatch at least three different kinds of actions!
 // - An action informing the reducers that the request began. (ex. toggling isLoggedIn in the state )
@@ -5,7 +14,12 @@
 // - An action informing the reducers that the request failed.
 
 
-// ✅ Async action creators using 'thunks!'
+// ✅ Async Flow
+// It provides a third-party extension point between dispatching an action, and the moment it reaches the reducer. 
+// ex. people use Redux middleware for logging, crash reporting, talking to an asynchronous API, routing, and more.
+
+
+// ✅ redux-thunk
 // ❓ how do we use the synchronous action creators we defined earlier together with network requests?
 // ❗ The standard way to do it with Redux is to use the Redux Thunk middleware.
 // ✨ The redux-thunk middleware, which allows simple asynchronous use of dispatch.
@@ -14,7 +28,3 @@
 // => This function doesn't need to be pure; it is thus allowed to have side effects, including executing asynchronous API calls.
 // => The function can also dispatch actions—like those synchronous actions we defined earlier.
 
-
-// ✅ Async Flow
-// It provides a third-party extension point between dispatching an action, and the moment it reaches the reducer. 
-// ex. people use Redux middleware for logging, crash reporting, talking to an asynchronous API, routing, and more.
