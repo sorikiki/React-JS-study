fd4f1dee926c62883f9948bbe8bc647c4f3d31c8
sorikiki <sorikiki98@naver.com>
2020-08-13 23:02:26 +0900

    javascript review_array

---
+++ javascript_review/array/array_methods.js
@@ -0,0 +1,13 @@
+// ✅ includes()
+// : The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+
+/* ex.
+    const array1 = [1, 2, 3];
+
+    console.log(array1.includes(2));
+    // expected output: true
+*/
+
+/* ex. To filter one array by comparing two arrays 💖
+    state.filter((project) => !action.key.includes(project.key));
+*/
