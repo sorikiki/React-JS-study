5b548c11eb4d928e88deeb3c34eb9f2997b611d7
sorikiki <sorikiki98@naver.com>
2020-08-16 17:56:27 +0900

    javascript review_async 2

--- javascript_review/async/async.js
+++ javascript_review/async/async.js
@@ -5,7 +5,6 @@
 /*
     function fetchUser() {
         //do network request in 10 secs...
-
         return 'dasol';
     }
 
@@ -56,6 +55,17 @@
 */
 
 // ✅ await
+// ✔ async/await syntax: 
+/*
+    async function 함수명() {
+    await 비동기_처리_메서드_명();
+}
+*/
+// => Attach 'await' before the asynchronous processing code that communicates HTTP among the internal logic of the function.
+// => ❗ Note: It is important to note that the asynchronous processing method must return the Promise object to work as intended by await.
+// => The asynchronous processing code, which is typically the target of await, is an API call function that returns a Promise, such as Axios.
+
+// ✅ Example 1: bring apple => banana => print both fruits
 /*
     function delay(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
@@ -67,26 +77,58 @@
     }
 
     async function getBanana() {
-        await delay(1000);
+        await delay(2000);
         return '🍌';
     }
 
-    // callback hell 😡
-
+    ✔ callback hell 😡
     function pickFruits() {
         return getApple().then(apple => {
             return getBanana().then(banana => `${apple}+${banana}`);
         })
     }
 
-    // async & await ✨
-
+    ✔ async & await 
     async function pickFruits() {
         const apple = await getApple();
         const banana = await getBanana();
         return `${apple}+${banana}`;
     }
-
-    // => it takes 2000s because it waits until promise finishes
+    // => it takes 3000ms because it waits until promise finishes
     
+    ✔ error handling: You can use catch {} in async, just as .catch() was used for error handling in the Promise.
+    async function pickFruits() {
+        try {
+            const apple = await getApple();
+            const banana = await getBanana();
+            return `${apple}+${banana}`;
+        } catch(error) {
+            console.log(error);
+        }
+        
+    ✔ parallel processing 1 (async/await)
+    async function pickFruits() {
+        const applePromise = getApple();
+        const bananaPromise = getBanana();
+        const apple = await applePromise;
+        const banana = await BananaPromise;
+        return `${apple}+${banana}`;
+    }
+    // => it takes 2000ms because when new Promise is created, the executor runs automatically.
+
+    ✔ parallel processing 2 (more useful Promise API)
+        1. Promise.all
+        function pickAllFruits() {
+            return Promise.all([getApple(), getBanana()])
+            .then(fruits => fruits.join(' + '));
+        }
+
+        pickAllFruits().then(console.log); // 🍎 + 🍌
+
+        2. Promise.race
+        function pickOnlyOne() {
+            return Promise.race([getApple(), getBanana()]);
+        }
+
+        pickOnlyOne().then(console.log); // 🍎
 */
