ebb1f71b57be87d9c6841f3ce444f89049674252
sorikiki <sorikiki98@naver.com>
2020-08-16 20:24:29 +0900

    JSON basic

--- javascript_review/async/4. axios.js
+++ javascript_review/async/4. axios.js
@@ -1,4 +1,4 @@
-// ✅ API call with axios 
+// ✅ API call with axios
 // review(async.js): The asynchronous processing code, which is typically the target of await, is an API call function that returns a Promise, such as Axios.
 // => This means that axios is an API call function that returns Promise!
 /*
@@ -54,3 +54,70 @@
     
     export default App;
 */
+
+// ✅ JSON: javascript object notation
+// => key-value pairs
+// => independent programming language and platform ✨
+
+//  ✔ Object to JSON
+// : object -> (serialize) -> string(JSON)
+// stringify(object)
+let json = JSON.stringify(true);
+console.log(json); // true
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json); // ["apple", "banana"]
+
+const rabbit = {
+    name: 'tori',
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump: () => {
+        console.log(`${this.name} can jump!`);
+    },
+}; // ❗ note that method is not a data so JSON do not contain this. + Symbol do so
+
+json = JSON.stringify(rabbit);
+console.log(json); // {"name": "tori", "color": "white", "size": null, "birthDate": "2020-05-29..."}
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'name' ? 'dasol' : value;
+});
+console.log(json);
+/*
+    key: , value: [object Object] // ❗ note that this top-ranked property prints at first.
+    key: name, value: tori
+    key: color, value: white
+    key: size, value: null
+    key: birthDate, value: ...
+    key: jump, value: () => { console.log(...) };
+    {"name": "dasol", "color": "white", ... }
+ */
+
+// ✔ JSON to Object
+// : string(JSON) -> (deserialize) -> object
+// parse(JSON)
+
+json = JSON.stringify(rabbit);
+let obj = JSON.parse(json);
+// ❗ note that object do not contain jump method because 'json' do not contain this at first.
+
+console.log(obj);
+console.log(obj.birthDate)
+// { ..., Date: "2020-05-29...", ... }
+// Type error
+// ❗ note that value of 'Date' is string type.
+
+obj = JSON.parse(json, (key, value) => {
+    return key === 'birthDate' ? new Date(value): value;
+})
+
+console.log(obj);
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
+// {..., Date: Fri May 29 ... , ... }
+// 29
+// 29
+
