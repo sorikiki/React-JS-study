ce5a645da04f02b66265a972828db613cc76424c
sorikiki <sorikiki98@naver.com>
2020-08-27 15:50:33 +0900

    refactoring practice

--- redux_project_todo_v2/src/Modules/index.js
+++ redux_project_todo_v2/src/Modules/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import user from './user';
 import todos from './todo';
 import counter from './counter';
+import posts from './posts';
 
 const reducer = combineReducers({
     user,
     todos,
     counter,
+    posts,
 });
 
 export default reducer;
---
+++ redux_project_todo_v2/src/lib/asyncUtils.js
@@ -0,0 +1,61 @@
+const createPromiseThunk = (type, promiseCreators) => dispatch  => {
+    const [ SUCCESS, ERROR ] = [`${type}_SUCCESS`, `${type}_ERROR`];
+    
+    // getPost에서 id를 받기에 param이 한 개 필요한 상황이다. 
+    return param => async dispatch =>
+    dispatch({ type, param });
+    try {
+        const payload = promiseCreators(param);
+        dispatch({ type: SUCCESS, payload });
+    } catch(e) {
+        dispatch({ type: ERROR, payload: e, error: true });
+    }
+}
+
+const reducerUtils = {
+    initial: (initialData = null) => ({
+        loading: false,
+        data: initialData,
+        error: null,
+    }),
+    loading: (prevState = null) => ({
+        loading: true,
+        data: prevState,
+        error: null,
+    }),
+    success: (payload) => ({
+        loading: false,
+        data: payload,
+        error:null,
+    }),
+    error: (error) => ({
+        loading: false,
+        data: null,
+        error: error,
+    })
+}
+
+export const handleAsyncActions = (type, key) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+    return (state, action) => {
+      switch (action.type) {
+        case type:
+          return {
+            ...state,
+            [key]: reducerUtils.loading()
+          };
+        case SUCCESS:
+          return {
+            ...state,
+            [key]: reducerUtils.success(action.payload)
+          };
+        case ERROR:
+          return {
+            ...state,
+            [key]: reducerUtils.error(action.payload)
+          };
+        default:
+          return state;
+      }
+    };
+  };
