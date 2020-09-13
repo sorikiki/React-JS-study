8e363956b0e6b3c5f378537ff86a5bf372e0acb0
sorikiki <sorikiki98@naver.com>
2020-09-13 17:16:06 +0900

    next.js example setup

---
+++ .react_advanced/ssr-practice/config/webpack.config.server.js
@@ -0,0 +1,13 @@
+const paths = require('./paths');
+
+module.export = {
+    mode: 'production',
+    entry: paths.ssrIndexJs,
+    target: 'node',
+    output: {
+        path: paths.ssrBuild,
+        filename: 'server.js',
+        chunkFilename: 'js/[name].chunk.js',
+        publicPath: paths.servedPath,
+    }
+}
---
+++ next_essentials/1. Overview.js
@@ -0,0 +1,4 @@
+// ✅ Next?
+// : The React Framework for Production
+// => Next.js gives you the best developer experience with all the features you need for production: 
+//    ex. hybrid static & server rendering, TypeScript support, smart bundling, route pre-fetching, and more. No config needed.
